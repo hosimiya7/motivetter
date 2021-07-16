dcec168606ed7bd87386a1735eedb0546b029b33
hosimiya7 <nerokarurin@gmail.com>
2021-07-16 13:59:33 +0900

    move_chara

--- components/MyCanvas.vue
+++ components/MyCanvas.vue
@@ -4,57 +4,87 @@
 
 <script>
 export default {
-    // プロップスの中身をデータに移す
-    props: {
-        _x: {
-            type: Number,
-            default: (750 - 150) / 2
-        },
-        _y: {
-            type: Number,
-            default: 150 - 75
-        },
-        /**
-         * @property　d 移動距離
-         */
-        d: {
-            type: Number,
-            default: 30
-        }
-    },
-    data(){
-        return{
+    data() {
+        return {
             screen_w: 750,
             screen_h: 150,
             chara_w: 150,
             chara_h: 75,
-            x: this._x,
-            is_left: Math.floor(Math.random() * 2) === 0,
-            is_right: !this.is_left
+            d: 30,
+            x: 0,
+            y: 0,
+            ctx: undefined,
+            is_left: true
         }
     },
     methods: {
+        // 初期化
+        init() {
+            this.x = (this.screen_w - this.chara_w) / 2
+            this.y = this.screen_h - this.chara_h
+            this.ctx = this.$el.getContext('2d')
+        },
+
         draw() {
+            //  キャラクターの描画
             this.ctx.clearRect(0, 0, this.screen_w, this.screen_h)
+
+            // 向きの決定
+            this.setCharaDirection()
+
+            // キャラクターの位置決定
+            if (this.is_left) {
+                if (this.isTouchWall()) {
+                    this.x += this.d
+                } else {
+                    this.x -= this.d
+                }
+            } else {
+                if (this.isTouchWall()) {
+                    this.x -= this.d
+                } else {
+                    this.x += this.d
+                }
+            }
+        },
+
+        // キャラクターが壁に当たるか判断
+        isTouchWall() {
+            if (this.is_left && this.x > 0) {
+                return false
+            }
+            if (!this.is_left && this.x < this.screen_w - this.chara_w) {
+                return false
+            }
+            return true
+        },
+        setCharaDirection(){
+            this.is_left = Math.floor(Math.random() * 2) === 0
             let chara = new Image();
-            chara.src = require("@/assets/imgs/moti_1_left.png");  // 画像のURLを指定
-            chara.onload = () => {
-                this.ctx.drawImage(chara, this.x, this._y, this.chara_w, this.chara_h)
-            }
-            if(this.x > 0){
-                this.x -= this.d
+
+            // 画像のURLを指定
+            if(this.is_left){
+                chara.src = require("@/assets/imgs/moti_1_left.png")
             }else{
-                this.x += this.d
+                chara.src = require("@/assets/imgs/moti_1_right.png")
             }
+
+            chara.onload = () => {
+                this.ctx.drawImage(chara, this.x, this.y, this.chara_w, this.chara_h)
+            }
+
         }
+
+
+
     },
     //   htmlに接続されたら
     mounted() {
         // mounted 以降で canvas の DOM にアクセスできる
         // CreateJS などを使うときにも、ここで canvas と紐付ける
         // console.log(this.$el)
-        this.ctx = this.$el.getContext('2d')
-        setInterval(this.draw, 800)
+        this.init()
+        setInterval(this.draw, 2000)
     }
 }
 </script>
